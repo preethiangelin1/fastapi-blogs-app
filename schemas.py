3a98d4f7b93e5fc38b536d34f680b25c4b5e3186
Preethi Angelin <preethiangelin1@gmail.com>
2026-09-09 22:15:07 +0530

    add schemas

---
+++ schemas.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class BlogCreate(BaseModel):
+    title: str
+    content: str
+    author: str
+    published: str
+
+class BlogResponse(BaseModel):
+    id: int
+    title: str
+    content: str
+    author: str
+    published: str
+    date_created: datetime
+    class Config():
+        orm_mode = True
+
+class BlogUpdate(BaseModel):
+    content: str | None = None
