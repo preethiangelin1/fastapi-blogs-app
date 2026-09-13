146e4f01113ca731bfe601435570de83ab6e9244
Preethi Angelin <preethiangelin1@gmail.com>
2026-09-13 21:03:08 +0530

    add CORS support for frontend

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from router import blog
 from db import models
 from db.database import engine
 
-
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(blog.router)
-
 models.Base.metadata.create_all(engine)
-
